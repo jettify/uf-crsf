3b5659c57114f2fc1e88d066ef2ea0a9ff6f0d05
Nikolay Novik <nickolainovik@gmail.com>
2025-06-24 19:26:57 -0400

    Update message timeout for example

--- examples/local_std.rs
+++ examples/local_std.rs
@@ -1,5 +1,6 @@
+use std::env;
+use std::io::ErrorKind;
 use std::time::Duration;
-use std::{env, io};
 
 fn main() {
     let ports = serialport::available_ports().expect("No ports found.");
@@ -8,17 +9,18 @@ fn main() {
     let path = env::args()
         .nth(1)
         .unwrap_or("/dev/tty.usbmodem00000000001B1".to_owned());
+
     let mut port = serialport::new(&path, 115_200)
-        .timeout(Duration::from_millis(2000))
+        .timeout(Duration::from_millis(10000))
         .open()
-        .expect("Can not open port.");
+        .expect("Can not open serial portport.");
 
     let mut buf = [0; 1024];
     loop {
         match port.read(buf.as_mut_slice()) {
-            Ok(n @ 1..) => println!("{:?}", &buf[..n]),
             Ok(0) => eprintln!("EOF"),
-            Err(err) if err.kind() == std::io::ErrorKind::TimedOut => eprint!("TimedOut"),
+            Ok(n @ 1..) => println!("{:?}", &buf[..n]),
+            Err(err) if err.kind() == ErrorKind::TimedOut => eprint!("TimedOut"),
             Err(err) => println!("{:?}", err),
         };
     }
