9fd4e8c684583d6fbb33eeb04d1b2ada910ce7e4
Nikolay Novik <nickolainovik@gmail.com>
2025-08-14 23:37:33 -0400

    More debuggable error variants.

--- src/error.rs
+++ src/error.rs
@@ -11,9 +11,10 @@ pub enum CrsfParsingError {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum CrsfStreamError {
-    InvalidSync,
-    InvalidPacketLength,
+    InvalidPacketLength(u8),
+    InvalidSync(u8),
     InvalidCrc { calculated_crc: u8, packet_crc: u8 },
     UnexpectedPacketType(u8),
     ParsingError(CrsfParsingError),
+    InputBufferTooSmall,
 }
