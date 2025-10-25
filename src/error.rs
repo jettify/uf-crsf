16f9796c808969ee228b76d2433ac2957e8bce70
jettify <nickolainovik@gmail.com>
2025-10-24 22:58:53 -0400

    feat: Implement embedded io traits integration. (#71)

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,6 @@
+#[cfg(any(feature = "async", feature = "blocking"))]
+use embedded_io::ErrorKind;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum CrsfParsingError {
@@ -13,8 +16,21 @@ pub enum CrsfParsingError {
 pub enum CrsfStreamError {
     InvalidPacketLength(u8),
     InvalidSync(u8),
-    InvalidCrc { calculated_crc: u8, packet_crc: u8 },
+    InvalidCrc {
+        calculated_crc: u8,
+        packet_crc: u8,
+    },
     UnexpectedPacketType(u8),
     ParsingError(CrsfParsingError),
     InputBufferTooSmall,
+    #[cfg(any(feature = "async", feature = "blocking"))]
+    Io(ErrorKind),
+    #[cfg(any(feature = "async", feature = "blocking"))]
+    UnexpectedEof,
+}
+
+impl From<CrsfParsingError> for CrsfStreamError {
+    fn from(e: CrsfParsingError) -> Self {
+        CrsfStreamError::ParsingError(e)
+    }
 }
