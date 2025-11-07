7ca6119d903d65f549489cf682bf14ad0d1461dc
Nikolay Novik <nickolainovik@gmail.com>
2025-11-07 18:46:22 -0500

    feat!: Rework parser output type from custom to Result<Option<>>.

--- examples/local_raw.rs
+++ examples/local_raw.rs
@@ -2,7 +2,6 @@ use std::io::{ErrorKind, Read};
 use std::process::exit;
 use std::time::Duration;
 use uf_crsf::packets::PacketType;
-use uf_crsf::parser::ParseResult;
 use uf_crsf::CrsfParser;
 
 fn main() {
@@ -26,12 +25,12 @@ fn main() {
                 for &byte in &buf[..n] {
                     let raw_packet_result = parser.push_byte_raw(byte);
                     match raw_packet_result {
-                        ParseResult::Complete(raw_packet) => {
+                        Ok(Some(raw_packet)) => {
                             let packet_type = PacketType::try_from(raw_packet.raw_packet_type());
                             println!("{:?} -> {:?}", packet_type, raw_packet);
                         }
-                        ParseResult::Error(e) => println!("Parsing error: {:?}", e),
-                        ParseResult::Incomplete => (),
+                        Ok(None) => (),
+                        Err(e) => println!("Parsing error: {:?}", e),
                     }
                 }
             }
